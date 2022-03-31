bedaa939d2f7eaa179b56c6f5b726e1b1f183d6c
Lucas Rhode <lucas.rhode@ul.com>
2022-03-31 14:54:05 -0300

    Fixes Q2 naming convention

--- Q2.java
+++ Q2.java
@@ -1,14 +1,14 @@
 public class Q2 {
     public static void main(String args[]) {
         for (int i = 1; i <= 100; i++) {
-            boolean multiple_5 = i % 5 == 0;
-            boolean multiple_3 = i % 3 == 0;
+            boolean multiple5 = i % 5 == 0;
+            boolean multiple3 = i % 3 == 0;
 
-            if (multiple_3 && multiple_5) {
+            if (multiple3 && multiple5) {
                 System.out.println("FooBaa");
-            } else if (multiple_3) {
+            } else if (multiple3) {
                 System.out.println("Foo");
-            } else if (multiple_5) {
+            } else if (multiple5) {
                 System.out.println("Baa");
             } else {
                 System.out.println(i);
