b876b5198109321daaa693b06ba49f763c4eac04
Lucas Rhode <lucas.rhode@ul.com>
2022-03-31 21:08:19 -0300

    Better Q9 message

--- com/src/Q9.java
+++ com/src/Q9.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Q9 {
     public static void main (String[] args) throws IOException {
-        String str = Utils.readCli("Enter any string:");
+        String str = Utils.readCli("Enter any string to count length:");
         System.out.println(str.getBytes().length);
     }
 }
